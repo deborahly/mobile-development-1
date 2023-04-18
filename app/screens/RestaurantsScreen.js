3e57c3de8734fe092657880daa42327c7fbbe826
Deborah Yscava <deborahly@gmail.com>
2023-04-18 18:23:41 -0400

    create RestaurantsScreen draft

---
+++ app/screens/RestaurantsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function RestaurantsScreen(props) {
+  return (
+    <SafeAreaView>
+      <Text>This is the Restaurant Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+export default RestaurantsScreen;
