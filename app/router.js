4ef1691570e1b453440c9f866bf53c254bc0e9aa
Deborah Yscava <deborahly@gmail.com>
2023-04-19 01:02:10 -0400

    implemented Tab

--- app/router.js
+++ app/router.js
@@ -1,28 +1,54 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useAuth } from './auth';
 import LoginScreen from './screens/LoginScreen';
 import RestaurantsScreen from './screens/RestaurantsScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import Header from './shared/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBurger } from '@fortawesome/free-solid-svg-icons/faBurger';
+import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons/faClockRotateLeft';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen
         name='Restaurants'
         component={RestaurantsScreen}
-        options={{ header: () => <Header /> }}
+        options={{
+          header: () => <Header />,
+          tabBarIcon: ({ focused }) => <FontAwesomeIcon icon={faBurger} />,
+        }}
       />
-    </Stack.Navigator>
+      <Tab.Screen
+        name='OrderHistory'
+        component={OrderHistoryScreen}
+        options={{
+          header: () => <Header />,
+          tabBarIcon: ({ focused }) => <FontAwesomeIcon icon={faClockRotateLeft} />,
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
 const AuthStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }}/>
+      <Stack.Screen
+        name='Login'
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
