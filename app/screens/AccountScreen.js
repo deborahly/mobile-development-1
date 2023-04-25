52e99b253998eb131f5da2c239060b0ad6b8537d
Deborah Yscava <deborahly@gmail.com>
2023-04-25 18:40:44 -0400

    create AccountScreen (for customers) create CourierAccountScreen

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import Account from '../components/Account';
+import customersUtils from '../utils/customersUtils';
+import { useAuth } from '../auth';
 
 function AccountScreen(props) {
-    return <SafeAreaView>This is Account</SafeAreaView>;
+  const [account, setAccount] = useState({});
+  const { authData } = useAuth();
+
+  useEffect(() => {
+    const fetchAccount = async id => {
+      const data = await customersUtils.getCustomer(id);
+      setAccount(data);
+    };
+    fetchAccount(authData.customer_id);
+  }, []);
+
+  return <Account accountType={'Customer'} account={account} />;
 }
 
 export default AccountScreen;
--- app/screens/CourierAccountScreen.js
+++ app/screens/CourierAccountScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import Account from '../components/Account';
+import couriersUtils from '../utils/couriersUtils';
+import { useAuth } from '../auth';
 
 function CourierAccountScreen(props) {
-  return <SafeAreaView>This is Courier Account</SafeAreaView>;
+  const [account, setAccount] = useState({});
+  const { authData } = useAuth();
+
+  useEffect(() => {
+    const fetchAccount = async id => {
+      const data = await couriersUtils.getCourier(id);
+      setAccount(data);
+    };
+    fetchAccount(authData.courier_id);
+  }, []);
+
+  return <Account accountType={'Courier'} account={account} />;
 }
 
 export default CourierAccountScreen;
