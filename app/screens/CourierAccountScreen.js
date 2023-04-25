33bce3f22f67c620eb304eb3242151967b6eccbc
Deborah Yscava <deborahly@gmail.com>
2023-04-24 20:12:10 -0400

    create component draft

---
+++ app/screens/CourierAccountScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+function CourierAccountScreen(props) {
+  return <SafeAreaView>This is Courier Account</SafeAreaView>;
+}
+
+export default CourierAccountScreen;
